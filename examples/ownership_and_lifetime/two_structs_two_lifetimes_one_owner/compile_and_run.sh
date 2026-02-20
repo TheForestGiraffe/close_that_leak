7354a4d1c9a751d7b884fbbd317d28231836ee06
pecavalc <pecavalc@student.42berlin.de>
2026-02-20 11:03:45 +0100

    Add valgrind to compile_and_run.sh script

--- examples/ownership_and_lifetime/two_structs_two_lifetimes_one_owner/compile_and_run.sh
+++ examples/ownership_and_lifetime/two_structs_two_lifetimes_one_owner/compile_and_run.sh
@@ -1,2 +1,8 @@
 clear
-cc -Wall -Wextra -Werror main.c app.c item.c && ./a.out
+cc -Wall -Wextra -Werror -g main.c app.c item.c && \
+#valgrind \
+#--leak-check=full \
+#--show-leak-kinds=all \
+#--track-origins=yes \
+#--track-fds=yes \
+./a.out
